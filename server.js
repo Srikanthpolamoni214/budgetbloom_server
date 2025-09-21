5e590a6efcd523810b427801557da42f89206ab7
Srikanthpolamoni214 <165095509+Srikanthpolamoni214@users.noreply.github.com>
2025-09-22 05:24:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use("/api/goalsTracker", goalsTrackerRoute);
 // Server
 const HOST = "0.0.0.0";
 app.listen(port, HOST,() => {
-  console.log(`✅ Server listening at http://${HOST}:${PORT}`);
+  console.log(`✅ Server listening at http://${HOST}:${port}`);
 });
 
 // Ensure data dir & file
